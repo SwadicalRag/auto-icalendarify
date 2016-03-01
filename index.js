dcf1431e93f214a5eefe1c841b9db39cf16c0e3f
Swad <SwadicalRag@users.noreply.github.com>
2016-03-01 17:50:27 +1030

    Things

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 'use strict';
 
-var legacy = require("./legacy.js");
+let legacy = require("./legacy.js");
+let EventEmitter = require('events').EventEmitter;
 
-
-
-module.exports = {
-    legacy: legacy
+let ttAPI = {
+    legacy: legacy,
+    emitter: new EventEmitter()
 };
+
+ttAPI.years = [];
+
+function validYear(year) {
+    for(let i=0;i < ttAPI.years.length;i++)
+        if(year == ttAPI.years[i])
+            return true;
+
+    return false;
+}
+
+ttAPI.helpers = {};
+
+ttAPI.emitter.emit("initialising");
+
+// tfw no promises
+ttAPI.legacy.getAvailableYears(function(err,yearData) {
+    for(let i=0;i < yearData.length;i++)
+        ttAPI.years[ttAPI.years.length] = yearData[i].DESCRIPTION;
+});
+
+// TODO: finish the high level API
+
+module.exports = ttAPI;
